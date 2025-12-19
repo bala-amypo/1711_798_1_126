32158d1f758f3bf402132c0f462e5513de731419
bala-amypo <bala@amypo.in>
2025-12-19 13:33:19 +0000

    User commit

--- src/main/java/com/example/demo/service/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/service/CustomUserDetailsService.java
@@ -1,17 +1,10 @@
 package com.example.demo.service;
 
-import com.example.demo.model.User;
-import com.example.demo.repository.UserRepository;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
+import com.example.demo.repository.UserRepository;
 
 @Service
-public class CustomUserDetailsService implements UserDetailsService {
+public class CustomUserDetailsService {
 
     private final UserRepository repository;
 
@@ -19,18 +12,7 @@ public class CustomUserDetailsService implements UserDetailsService {
         this.repository = repository;
     }
 
-    @Override
-    public UserDetails loadUserByUsername(String username)
-            throws UsernameNotFoundException {
-
-        User user = repository.findByUsername(username)
-                .orElseThrow(() ->
-                        new UsernameNotFoundException("User not found: " + username));
-
-        return new org.springframework.security.core.userdetails.User(
-                user.getUsername(),
-                user.getPassword(),
-                List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole()))
-        );
+    public UserRepository getRepository() {
+        return repository;
     }
 }
