39448e89084446cf4844a9bea8ccc8262d70701f
bala-amypo <bala@amypo.in>
2025-12-21 14:16:54 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -0,0 +1,12 @@
+package com.example.demo.dto;
+import lombok.Data;
+
+@Data
+public class AuthResponse {
+    private String accessToken;
+    private String tokenType = "Bearer";
+
+    public AuthResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
