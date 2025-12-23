ea2cfb52e8a11c7e8669842feaf3252c9b608302
bala-amypo <bala@amypo.in>
2025-12-23 04:36:33 +0000

    User commit

--- src/main/java/com/example/demo/repository/PurchaseRecordRepository.java
+++ src/main/java/com/example/demo/repository/PurchaseRecordRepository.java
@@ -3,7 +3,11 @@ package com.example.demo.repository;
 import com.example.demo.model.PurchaseRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface PurchaseRecordRepository extends JpaRepository<PurchaseRecord, Long> {
+    
+    // Indha line-ah mattum add pannunga, appo dhaan error pogum
+    List<PurchaseRecord> findByCustomerBusinessId(String customerId);
 }
