d8fff3c062e2215cdb0965bcdf6720c844b9beac
bala-amypo <bala@amypo.in>
2025-12-26 08:46:58 +0000

    User commit

--- src/main/java/com/example/demo/service/PurchaseRecordService.java
+++ src/main/java/com/example/demo/service/PurchaseRecordService.java
@@ -1,19 +1,7 @@
-/*package com.example.demo.service;
 
-import com.example.demo.model.PurchaseRecord;
-import java.util.List;
 
-public interface PurchaseRecordService {
 
-    PurchaseRecord recordPurchase(PurchaseRecord purchase);
 
-    List<PurchaseRecord> getPurchasesByCustomer(Long customerId);
-
-    List<PurchaseRecord> getAllPurchases();
-
-    PurchaseRecord getPurchaseById(Long id);
-}
-*/
 package com.example.demo.service;
 
 import com.example.demo.model.PurchaseRecord;
